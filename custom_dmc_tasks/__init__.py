99fad8525eef349a68552e4b52e55e211077d5ee
mazpie <pietromazzaglia@gmail.com>
2023-07-12 09:58:25 +0900

    Updated conda env deps. Removed unused env deps.

--- custom_dmc_tasks/__init__.py
+++ custom_dmc_tasks/__init__.py
@@ -1,6 +1,4 @@
-from custom_dmc_tasks import cheetah
 from custom_dmc_tasks import walker
-from custom_dmc_tasks import hopper
 from custom_dmc_tasks import quadruped
 from custom_dmc_tasks import jaco
 
@@ -10,18 +8,8 @@ def make(domain, task,
          environment_kwargs=None,
          visualize_reward=False):
     
-    if domain == 'cheetah':
-        return cheetah.make(task,
-                            task_kwargs=task_kwargs,
-                            environment_kwargs=environment_kwargs,
-                            visualize_reward=visualize_reward)
-    elif domain == 'walker':
+    if domain == 'walker':
         return walker.make(task,
-                           task_kwargs=task_kwargs,
-                           environment_kwargs=environment_kwargs,
-                           visualize_reward=visualize_reward)
-    elif domain == 'hopper':
-        return hopper.make(task,
                            task_kwargs=task_kwargs,
                            environment_kwargs=environment_kwargs,
                            visualize_reward=visualize_reward)
